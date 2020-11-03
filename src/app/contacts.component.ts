d2105e2f02cc2ac1147b1d6a9fbac7a2a1757bd2
Valentin <perso@valentinbru.com>
2020-11-03 17:38:56 +0100

    ajout loop for contacts template and array

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -7,9 +7,7 @@ import {Contact} from "./Contact";
       <h4>Nb Contact: {{ numberOfContacts }}</h4>
       <form>
         <ul>
-          <cnt-contact [contact]="contact"></cnt-contact>
-          <cnt-contact [contact]="contact2"></cnt-contact>
-          <cnt-contact [contact]="contact3"></cnt-contact>
+          <cnt-contact *ngFor="let contact of contactArray" [contact]="contact"></cnt-contact>
         </ul>
       </form>
     
@@ -23,21 +21,20 @@ export class ContactsComponent implements OnInit {
   constructor() { }
 
   numberOfContacts = 3;
-  contact:Contact = {
+  contactArray:Contact[] =[{
     id:0,
     firstName: 'Jack', lastName: 'Bauer',
     email: 'jack.bauer@fbi.com'
-  };
-  contact2:Contact = {
+  },{
     id:1,
     firstName: 'Jack', lastName: 'lang',
     email: 'jack.lang@fbi.com'
-  };
-  contact3:Contact = {
+  },{
     id:2,
     firstName: 'Jack', lastName: 'ichan',
     email: 'jack.ichan@fbi.com'
-  };
+  } ];
+  
 
   ngOnInit(): void {
   }
